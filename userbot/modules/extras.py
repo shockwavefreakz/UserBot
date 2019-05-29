3f6546dc6747c7202a3e78169665a5f77b260dba
thatspix <50486055+thatspix@users.noreply.github.com>
2019-05-29 18:20:50 +0530

    Update extras.py

--- userbot/modules/extras.py
+++ userbot/modules/extras.py
@@ -162,13 +162,6 @@ async def rextestercli(e):
 
         await e.edit(output)
 
-@register(outgoing=True, pattern="^.repeat")
-async def inlinespem(e):
-    message = e.text[8:-4]
-    count = int(e.text[-3:])
-    final = message * count
-    await e.respond(final)
-
 
 
 HELPER.update({
